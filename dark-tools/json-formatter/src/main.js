06beba90f0994ddcd2616bf693ca27895fc01948
Naman Monga <naman.monga.i@dotpe.in>
2022-03-21 00:21:29 +0530

    autoremove backslash & copy button added

--- dark-tools/json-formatter/src/main.js
+++ dark-tools/json-formatter/src/main.js
@@ -1,47 +1,76 @@
 /* ======= Constants ======= */
-const inputArea = document.querySelector(".large-area--input");
-const btnFormat = document.querySelector(".controls__button--format");
-const btnMinify = document.querySelector(".controls__button--minify");
-const popup = document.getElementById("myPopup");
+const inputArea        = document.querySelector(".large-area--input");
+const btnFormat        = document.querySelector(".controls__button--format");
+const btnMinify        = document.querySelector(".controls__button--minify");
+const btnCopy          = document.querySelector(".controls__button--copy");
+const invalidJsonPopup = document.getElementById("invalidJsonPopup");
+const jsonCopiedPopup  = document.getElementById("jsonCopiedPopup");
 
 
 /* ======= Event Listeners ======= */
-btnFormat.addEventListener("click", () =>     
+btnFormat.addEventListener("click", () =>
     {
         var inputText = inputArea.textContent
-        if (!isJsonString(inputText)){
-            console.log("Error invalid JSON: "+ inputText)
-            popup.classList.add("show");
-            return
+        if (!isJson(inputText)){
+            var jsonText = stringToJson(inputText)
+            if(jsonText != ""){
+                inputText = jsonText
+            } else {
+                console.log("Error invalid JSON: "+ inputText)
+                invalidJsonPopup.classList.add("show");
+                return
+            }
         }
-        popup.classList.remove("show");
+        invalidJsonPopup.classList.remove("show");
         var formatted = JSON.stringify(JSON.parse(inputText), null, 4)
         formatted = hljs.highlight(formatted, {language: 'json', style:'atom-one-dark'}).value
         inputArea.innerHTML = formatted
     }
 )
 
-btnMinify.addEventListener("click", () =>     
+btnMinify.addEventListener("click", () =>
     {
         var inputText = inputArea.textContent
-        if (!isJsonString(inputText)){
-            console.log("Error invalid JSON: "+ inputText)
-            popup.classList.add("show");
-            return
+        if (!isJson(inputText)){
+            var jsonText = stringToJson(inputText)
+            if(jsonText != ""){
+                inputText = jsonText
+            } else {
+                console.log("Error invalid JSON: "+ inputText)
+                invalidJsonPopup.classList.add("show");
+                return
+            }
         }
-        popup.classList.remove("show");
+        invalidJsonPopup.classList.remove("show");
         var formatted = JSON.stringify(JSON.parse(inputText))
         formatted = hljs.highlight(formatted, {language: 'json', style:'atom-one-dark'}).value
         inputArea.innerHTML = formatted
     }
 )
 
+btnCopy.addEventListener("click", () =>
+{
+    jsonCopiedPopup.classList.toggle("show");
+    setTimeout(() => {
+        jsonCopiedPopup.classList.toggle("show") 
+    }, 3000);
+    navigator.clipboard.writeText(inputArea.textContent);
+})
+
 /* ======= Helper Functions ======= */
-function isJsonString(str) {
+function isJson(str) {
     try {
         JSON.parse(str);
     } catch (e) {
         return false;
     }
     return true;
+}
+
+function stringToJson(str) {
+    str = str.replaceAll('\\', '')
+    if (!isJson(str)){
+        return ""
+    }
+    return str
 }
