5a357860e5f05ccffeb06b854f3ad18fa5878a97
Naman Monga <naman.monga.i@dotpe.in>
2022-03-20 22:37:13 +0530

    added prompt on invalid json and some minor bug fixes

--- dark-tools/json-formatter/src/main.js
+++ dark-tools/json-formatter/src/main.js
@@ -2,16 +2,19 @@
 const inputArea = document.querySelector(".large-area--input");
 const btnFormat = document.querySelector(".controls__button--format");
 const btnMinify = document.querySelector(".controls__button--minify");
+const popup = document.getElementById("myPopup");
 
 
 /* ======= Event Listeners ======= */
 btnFormat.addEventListener("click", () =>     
     {
-        var inputText = inputArea.textContent.trim()
+        var inputText = inputArea.textContent
         if (!isJsonString(inputText)){
-            console.log("Error invalid JSON: "+inputArea.inputText)
+            console.log("Error invalid JSON: "+ inputText)
+            popup.classList.add("show");
             return
         }
+        popup.classList.remove("show");
         var formatted = JSON.stringify(JSON.parse(inputText), null, 4)
         formatted = hljs.highlight(formatted, {language: 'json', style:'atom-one-dark'}).value
         inputArea.innerHTML = formatted
@@ -20,11 +23,13 @@ btnFormat.addEventListener("click", () =>
 
 btnMinify.addEventListener("click", () =>     
     {
-        var inputText = inputArea.textContent.trim()
+        var inputText = inputArea.textContent
         if (!isJsonString(inputText)){
-            console.log("Error invalid JSON: "+inputText)
+            console.log("Error invalid JSON: "+ inputText)
+            popup.classList.add("show");
             return
         }
+        popup.classList.remove("show");
         var formatted = JSON.stringify(JSON.parse(inputText))
         formatted = hljs.highlight(formatted, {language: 'json', style:'atom-one-dark'}).value
         inputArea.innerHTML = formatted
